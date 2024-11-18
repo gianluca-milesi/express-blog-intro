c0ac21b6df9739e8324eeefa259e2cfe610432c9
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-18 15:28:22 +0100

    ADD: get and listen methods

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("ciao");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Server del mio blog");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server port: ${port}`);
+});
