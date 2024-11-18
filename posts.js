3fa071a6b9f191740f771f9f3269a22d091a09a6
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-18 16:11:53 +0100

    Fixed paths in url properties

--- posts.js
+++ posts.js
@@ -2,31 +2,31 @@ const posts = [
     {
         titolo: "Ciambellone",
         contenuto: "il ciambellone di mia nonna",
-        url: "./public/images/ciambellone.jpeg",
+        url: "/images/ciambellone.jpeg",
         tags: ["ciambellone", "nonna", "cucina"]
     },
     {
         titolo: "Cracker Barbabietola",
         contenuto: "Strani ma buoni",
-        url: "./public/images/cracker_barbabietola.jpeg.jpeg",
+        url: "/images/cracker_barbabietola.jpeg.jpeg",
         tags: ["cracker", "barbabietola", "cucina"]
     },
     {
         titolo: "Pane Fritto",
         contenuto: "Dolcissimo",
-        url: "public/images/pane_fritto_dolce.jpeg",
+        url: "/images/pane_fritto_dolce.jpeg",
         tags: ["pane", "fritto", "cucina"]
     },
     {
         titolo: "Pasta Barbabietola",
         contenuto: "Da assaggiare",
-        url: "public/images/pasta_barbabietola.jpeg",
+        url: "/images/pasta_barbabietola.jpeg",
         tags: ["pasta", "barbabietola", "cucina"]
     },
     {
         titolo: "Torta Paesana",
         contenuto: "Fatta in casa",
-        url: "public/images/torta_paesana.jpeg",
+        url: "/images/torta_paesana.jpeg",
         tags: ["torta", "paesana", "cucina"]
     },
 ];
