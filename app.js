12b6b9aa40f903cc5f1b06437787cc67e241d847
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-18 15:41:07 +0100

    ADD: posts array

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = require("./posts.js");
 
 app.get("/", (req, res) => {
     res.send("Server del mio blog");
---
+++ posts.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "il ciambellone di mia nonna",
+        url: "./public/images/ciambellone.jpeg",
+        tags: ["ciambellone", "nonna", "cucina"]
+    },
+    {
+        titolo: "Cracker Barbabietola",
+        contenuto: "Strani ma buoni",
+        url: "./public/images/cracker_barbabietola.jpeg.jpeg",
+        tags: ["cracker", "barbabietola", "cucina"]
+    },
+    {
+        titolo: "Pane Fritto",
+        contenuto: "Dolcissimo",
+        url: "public/images/pane_fritto_dolce.jpeg",
+        tags: ["pane", "fritto", "cucina"]
+    },
+    {
+        titolo: "Pasta Barbabietola",
+        contenuto: "Da assaggiare",
+        url: "public/images/pasta_barbabietola.jpeg",
+        tags: ["pasta", "barbabietola", "cucina"]
+    },
+    {
+        titolo: "Torta Paesana",
+        contenuto: "Fatta in casa",
+        url: "public/images/torta_paesana.jpeg",
+        tags: ["torta", "paesana", "cucina"]
+    },
+];
+
+module.exports = posts;
